5f08cbbec54c3b21e75312947b2b8f783fd54973
Travis Plunk <github@ez13.net>
2019-08-06 11:01:22 -0700

    Add get-dscresource test
    # Conflicts:
    #	test/PSDesiredStateConfiguration.Tests.ps1

--- test/PSDesiredStateConfiguration.Tests.ps1
+++ test/PSDesiredStateConfiguration.Tests.ps1
@@ -1,27 +1,40 @@
 Describe "Test PSDesiredStateConfiguration" -tags CI {
-    BeforeAll {
-        $commands = Get-Command -Module PSDesiredStateConfiguration
-    }
-    BeforeEach {
-    }
-    AfterEach {
-    }
-    AfterAll {
-    }
-
-    It "The module should have 3 commands" {
-        if($commands.Count -ne 3)
-        {
-            $modulePath = (Get-Module PSDesiredStateConfiguration).Path
-            Write-Verbose -Verbose -Message "PSDesiredStateConfiguration Path: $modulePath"
-            $commands | Out-String | Write-Verbose -Verbose
+    Context "Module loading" {
+        BeforeAll {
+            $commands = Get-Command -Module PSDesiredStateConfiguration
+            $expectedCommandCount = 3
         }
-        $commands.Count | Should -Be 3
+        BeforeEach {
+        }
+        AfterEach {
+        }
+        AfterAll {
+        }
+
+        It "The module should have $expectedCommandCount commands" {
+            if($commands.Count -ne $expectedCommandCount)
+            {
+                $modulePath = (Get-Module PSDesiredStateConfiguration).Path
+                Write-Verbose -Verbose -Message "PSDesiredStateConfiguration Path: $modulePath"
+                $commands | Out-String | Write-Verbose -Verbose
+            }
+            $commands.Count | Should -Be $expectedCommandCount
+        }
+        It "The module should have the Configuration Command" {
+            $commands | Where-Object {$_.Name -eq 'Configuration'} | Should -Not -BeNullOrEmpty
+        }
+        It "The module should have the Get-DscResource Command" {
+            $commands | Where-Object {$_.Name -eq 'Get-DscResource'} | Should -Not -BeNullOrEmpty
+        }
     }
-    It "The module should have the Configuration Command" {
-        $commands | Where-Object {$_.Name -eq 'Configuration'} | Should -Not -BeNullOrEmpty
-    }
-    It "The module should have the Get-DscResource Command" {
-        $commands | Where-Object {$_.Name -eq 'Get-DscResource'} | Should -Not -BeNullOrEmpty
+    Context "Get-DscResource" {
+        it "should be able to get a resource without module name" -Pending {
+            $resource =Get-DscResource -Name PsModule
+            $resource | Should -Not -BeNullOrEmpty
+        }
+        it "should be able to get a resource with module name" {
+            $resource =Get-DscResource -Name PsModule -Module PowerShellGet
+            $resource | Should -Not -BeNullOrEmpty
+        }
     }
 }
