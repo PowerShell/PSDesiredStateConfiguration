da9b73553e4d273d3c8830b1adff419a82ba79d8
Travis Plunk <travis.plunk@microsoft.com>
2019-08-06 17:13:19 -0700

    Add get-dscresource test (#10)

--- test/PSDesiredStateConfiguration.Tests.ps1
+++ test/PSDesiredStateConfiguration.Tests.ps1
@@ -1,27 +1,44 @@
 Describe "Test PSDesiredStateConfiguration" -tags CI {
-    BeforeAll {
-        $commands = Get-Command -Module PSDesiredStateConfiguration
-    }
-    BeforeEach {
-    }
-    AfterEach {
-    }
-    AfterAll {
-    }
-
-    It "The module should have 3 commands" {
-        if($commands.Count -ne 3)
-        {
-            $modulePath = (Get-Module PSDesiredStateConfiguration).Path
-            Write-Verbose -Verbose -Message "PSDesiredStateConfiguration Path: $modulePath"
-            $commands | Out-String | Write-Verbose -Verbose
+    Context "Module loading" {
+        BeforeAll {
+            $commands = Get-Command -Module PSDesiredStateConfiguration
+            $expectedCommandCount = 3
         }
-        $commands.Count | Should -Be 3
+        BeforeEach {
+        }
+        AfterEach {
+        }
+        AfterAll {
+        }
+
+        It "The module should have $expectedCommandCount commands" {
+            if($commands.Count -ne $expectedCommandCount)
+            {
+                $modulePath = (Get-Module PSDesiredStateConfiguration).Path
+                Write-Verbose -Verbose -Message "PSDesiredStateConfiguration Path: $modulePath"
+                $commands | Out-String | Write-Verbose -Verbose
+            }
+            $commands.Count | Should -Be $expectedCommandCount
+        }
+        It "The module should have the Configuration Command" {
+            $commands | Where-Object {$_.Name -eq 'Configuration'} | Should -Not -BeNullOrEmpty
+        }
+        It "The module should have the Get-DscResource Command" {
+            $commands | Where-Object {$_.Name -eq 'Get-DscResource'} | Should -Not -BeNullOrEmpty
+        }
     }
-    It "The module should have the Configuration Command" {
-        $commands | Where-Object {$_.Name -eq 'Configuration'} | Should -Not -BeNullOrEmpty
-    }
-    It "The module should have the Get-DscResource Command" {
-        $commands | Where-Object {$_.Name -eq 'Get-DscResource'} | Should -Not -BeNullOrEmpty
+    Context "Get-DscResource" {
+        # https://github.com/PowerShell/PSDesiredStateConfiguration/issues/11
+        it "should be able to get a resource without module name" -Pending {
+            $resource =Get-DscResource -Name PsModule
+            $resource | Should -Not -BeNullOrEmpty
+        }
+
+        # Linux issue: https://github.com/PowerShell/PSDesiredStateConfiguration/issues/12
+        # macOS issue: https://github.com/PowerShell/MMI/issues/33
+        it "should be able to get a resource with module name" -skip:($IsMacOS -or $IsLinux) {
+            $resource =Get-DscResource -Name PsModule -Module PowerShellGet
+            $resource | Should -Not -BeNullOrEmpty
+        }
     }
 }
