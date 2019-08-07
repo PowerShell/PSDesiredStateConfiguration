2b48bb946953a6e382a81a71a61f75ffb7b4856b
Travis Plunk <github@ez13.net>
2019-08-07 14:45:29 -0700

    add test cases

--- test/PSDesiredStateConfiguration.Tests.ps1
+++ test/PSDesiredStateConfiguration.Tests.ps1
@@ -29,15 +29,37 @@ Describe "Test PSDesiredStateConfiguration" -tags CI {
     }
     Context "Get-DscResource" {
         # https://github.com/PowerShell/PSDesiredStateConfiguration/issues/11
-        it "should be able to get a resource without module name" -Pending {
-            $resource =Get-DscResource -Name PsModule
+        BeforeAll {
+            $testCases = @(
+                @{
+                    TestCaseName = 'case mismatch in resource name'
+                    Name = 'PsModule'
+                    ModuleName = 'PowerShellGet'
+                }
+                @{
+                    TestCaseName = 'Good'
+                    Name = 'PSModule'
+                    ModuleName = 'PowerShellGet'
+                }
+                @{
+                    TestCaseName = 'case mismatch in module name'
+                    Name = 'PSModule'
+                    ModuleName = 'powershellget'
+                }
+            )
+
+        }
+        it "should be able to get a <Name> - <TestCaseName>" -TestCases $testCases  {
+            param($Name)
+            $resource =Get-DscResource -Name $name
             $resource | Should -Not -BeNullOrEmpty
         }
 
         # Linux issue: https://github.com/PowerShell/PSDesiredStateConfiguration/issues/12
         # macOS issue: https://github.com/PowerShell/MMI/issues/33
-        it "should be able to get a resource with module name" -skip:($IsMacOS -or $IsLinux) {
-            $resource =Get-DscResource -Name PsModule -Module PowerShellGet
+        it "should be able to get a <Name> from <ModuleName> - <TestCaseName>" -TestCases $testCases  {
+            param($Name,$ModuleName)
+            $resource =Get-DscResource -Name $Name -Module $ModuleName
             $resource | Should -Not -BeNullOrEmpty
         }
     }
