ac1ce06e3e6a763032391535951bb05ac209b93a
Travis Plunk <github@ez13.net>
2019-09-10 15:02:41 -0700

    specify module version

--- test/configuration.Tests.ps1
+++ test/configuration.Tests.ps1
@@ -1,13 +1,18 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 Describe "DSC MOF Compilation" -tags "CI" {
+    BeforeAll {
+        $module = Get-Module PowerShellGet -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
+
+        $psGetModuleVersion = $module.Version.ToString()
+    }
 
     It "Should be able to compile a MOF using PSModule resource"  {
         Write-Verbose "DSC_HOME: ${env:DSC_HOME}" -verbose
         [Scriptblock]::Create(@"
         configuration DSCTestConfig
         {
-            Import-DscResource -ModuleName PowerShellGet
+            Import-DscResource -ModuleName PowerShellGet -ModuleVersion $psGetModuleVersion
             Node "localhost" {
                 PSModule f1
                 {
