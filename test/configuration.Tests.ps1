f3bb6fed5f50ce47a4c061c2b1b6b8b81a6ca174
Travis Plunk <github@ez13.net>
2019-09-17 12:20:51 -0700

    Add a better fix

--- test/configuration.Tests.ps1
+++ test/configuration.Tests.ps1
@@ -5,7 +5,10 @@ Describe "DSC MOF Compilation" -tags "CI" {
         $module = Get-Module PowerShellGet -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
 
         $psGetModuleVersion = $module.Version.ToString()
-        #Import-Module PSDesiredStateConfiguration
+        if (!$env:DSC_HOME)
+        {
+            Import-Module PSDesiredStateConfiguration
+        }
     }
 
     It "Should be able to compile a MOF using PSModule resource"  {
