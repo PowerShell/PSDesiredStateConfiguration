e3ec5c52d980a7864f023abe23735191dd4bab12
Andrew <anmenaga@microsoft.com>
2021-06-01 20:39:48 -0700

    Build update related to compiling subsystem dll (#49)

--- dobuild.ps1
+++ dobuild.ps1
@@ -13,17 +13,14 @@ Implement build and packaging of the package and place the output $OutDirectory/
 function DoBuild
 {
     Write-Verbose -Verbose -Message "Starting DoBuild"
+    Write-Verbose -Verbose -Message "Make sure that 'nuget' and 'dotnet' are visible through PATH"
     
     Write-Verbose -Verbose -Message "Copying module files to '${OutDirectory}/${ModuleName}'"
     # copy psm1 and psd1 files
     copy-item "${SrcPath}/*" "${OutDirectory}/${ModuleName}" -Recurse
 
-    # copy help
-    # Write-Verbose -Verbose -Message "Copying help files to '${OutDirectory}/${ModuleName}'"
-    # copy-item -Recurse "${HelpPath}/${Culture}" "${OutDirectory}/${ModuleName}"
-
-    $subsystemCodePath = Resolve-Path "${SrcPath}\..\DscSubsystem"
-    $subsystemBinPath = "bin/Debug/net6.0/publish/Microsoft.PowerShell.DscSubsystem.dll"
+    $smaPackageVersionToUse = "7.2.0-preview.6" # 7.2.0-preview.6 - is the first SMA version that has DSC subsystem changes
+    $subsystemCodePath = Resolve-Path "${SrcPath}/../DscSubsystem"
     Write-Verbose -Verbose -Message "Subsystem code path ${subsystemCodePath}"
 
     if ( Test-Path $subsystemCodePath )
@@ -31,22 +28,27 @@ function DoBuild
         Write-Verbose -Verbose -Message "Building assembly and copying to '${OutDirectory}/${ModuleName}'"
         
         Push-Location $subsystemCodePath
-        $result = dotnet publish
+
+        $PackageReferencesPath = Join-Path $subsystemCodePath "PackageReferences"
+
+        nuget install System.Management.Automation -OutputDirectory ./PackageReferences -PreRelease -Version $smaPackageVersionToUse -DependencyVersion Ignore -ExcludeVersion
+        dotnet publish
+
+        $subsystemBinPath = Join-Path (Get-ChildItem -Recurse "publish" -Directory) "Microsoft.PowerShell.DscSubsystem.dll"
         if (Test-Path $subsystemBinPath)
         {
             Copy-Item $subsystemBinPath "${OutDirectory}/${ModuleName}"
         }
         else
         {
-            Write-Error "dotnet build failed - $subsystemBinPath not found - $result"
+            Write-Error -Message "dotnet build failed - $subsystemBinPath not found"
         }
 
         Pop-Location
     }
     else {
-        Write-Verbose -Verbose -Message "No code to build in '$subsystemCodePath'"
+        Write-Error -Message "No code to build in '$subsystemCodePath'"
     }
 
-    ## Add build and packaging here
     Write-Verbose -Verbose -Message "Ending DoBuild"
 }
